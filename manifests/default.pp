bce7c1e4bc572ecf8d2849fd46818b9740fdfd70
corvus_ch <christian.haeusler@md-systems.ch>
2013-08-08 11:58:21 +0200

    Added mysql

--- manifests/default.pp
+++ manifests/default.pp
@@ -2,4 +2,14 @@
 class {'apache': 
   mpm_module => prefork
 }
-include apache::mod::php
+include apache::mod::php  
+
+include mysql
+include mysql::php
+include mysql::server
+
+mysql::db {'vagrant':
+  user => 'vagrant',
+  password => 'vagrant',
+}
+
